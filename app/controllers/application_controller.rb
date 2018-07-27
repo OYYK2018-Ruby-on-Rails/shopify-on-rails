575a52b3b95d969a2d0dbc2281f99f2e1e7e4d79
OguzBayat <oguzbayat77@gmail.com>
2018-07-27 12:29:34 +0300

    another fix for seeding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+	before_action :zero_error
   def seed
     Rails.application.load_seed
     redirect_to root_path
+  end
+
+  private
+  def zero_error
+  	@zeros = Variant.where('stock < (?)', 1).destroy_all
   end
 end
