064d68aa20f7809fc6aa870ea6d65652d89fe3fb
hayrettine <hayrettine@anadolu.edu.tr>
2018-07-27 15:25:41 +0300

    fix variant destroy_all

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :variants, dependent: :destroy
   accepts_nested_attributes_for :variants
+  scope :available, -> {where('stock > 0')}
   validates :name, uniqueness: true, presence: true
   validates :price, numericality: {greater_than: 0}
 
